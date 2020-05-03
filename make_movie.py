c345d124ebb915ffbb4bd5c80ee4af1eb0013d49
Thomas Denewiler <tdenewiler@gmail.com>
2020-05-03 14:39:21 -0700

    Static analysis cleanup (#5)
    * Added --check flag to statick so that discovered issues will cause the command to return a non-zero exit code.
    
    * Minor fixes for pylint. Applied black formatting. Reduced statick discovery plugins to run.
    
    * Removed statick markdown discovery.

--- make_movie.py
+++ make_movie.py
@@ -18,57 +18,96 @@ from mutagen.mp3 import MP3  # pylint: disable=import-error
 from progress.bar import Bar  # pylint: disable=import-error
 
 
-class MakeMovie():
+class MakeMovie:
     """Make a movie using image files."""
 
     def __init__(self):
         """Get options and start making a movie."""
         parser = argparse.ArgumentParser()
-        parser.add_argument("-s", "--source_dir", dest="source_directory",
-                            help="Directory containing original images. \
-                                  Default to current directory.", default='.')
-        parser.add_argument("-t", "--tmp_dir", dest="tmp_directory",
-                            help="Directory containing temporary images. "
-                                 "Default to tmp.", default='tmp/')
-        parser.add_argument("-f", "--frame_rate", dest="fps",
-                            help="Frames per second for the output movie. "
-                            "Default to 2.", default=2)
-        parser.add_argument("-o", "--output_filename", dest="output_filename",
-                            help="Output filename. Default to movie.",
-                            default='movie.mkv')
-        parser.add_argument("-w", "--max_width", dest="max_width",
-                            help="Maximum width of output video. Default to "
-                            "1920.", default=1920)
-        parser.add_argument("-e", "--max_height", dest="max_height",
-                            help="Maximum height of output video. Default to "
-                            "1080.", default=1080)
-        parser.add_argument("-m", "--music", dest="music",
-                            help="Name of audio file to add to video.",
-                            default=None)
-        parser.add_argument("-k", "--skip", dest="skip",
-                            help="Skip scaling images and use existing "
-                            "temporary images.", default=False)
+        parser.add_argument(
+            "-s",
+            "--source_dir",
+            dest="source_directory",
+            help="Directory containing original images. \
+                                  Default to current directory.",
+            default=".",
+        )
+        parser.add_argument(
+            "-t",
+            "--tmp_dir",
+            dest="tmp_directory",
+            help="Directory containing temporary images. " "Default to tmp.",
+            default="tmp/",
+        )
+        parser.add_argument(
+            "-f",
+            "--frame_rate",
+            dest="fps",
+            help="Frames per second for the output movie. " "Default to 2.",
+            default=2,
+        )
+        parser.add_argument(
+            "-o",
+            "--output_filename",
+            dest="output_filename",
+            help="Output filename. Default to movie.",
+            default="movie.mkv",
+        )
+        parser.add_argument(
+            "-w",
+            "--max_width",
+            dest="max_width",
+            help="Maximum width of output video. Default to " "1920.",
+            default=1920,
+        )
+        parser.add_argument(
+            "-e",
+            "--max_height",
+            dest="max_height",
+            help="Maximum height of output video. Default to " "1080.",
+            default=1080,
+        )
+        parser.add_argument(
+            "-m",
+            "--music",
+            dest="music",
+            help="Name of audio file to add to video.",
+            default=None,
+        )
+        parser.add_argument(
+            "-k",
+            "--skip",
+            dest="skip",
+            help="Skip scaling images and use existing " "temporary images.",
+            default=False,
+        )
         options = parser.parse_args()
         fps = options.fps
 
         image_names = self.get_image_names(options.source_directory)
 
-        size = self.get_max_image_resolution(image_names,
-                                             options.max_width,
-                                             options.max_height)
-        print('Found {} images and setting resolution to (w, h) = ({}, {}).'.
-              format(len(image_names), size[0], size[1]))
+        size = self.get_max_image_resolution(
+            image_names, options.max_width, options.max_height
+        )
+        print(
+            "Found {} images and setting resolution to (w, h) = ({}, {}).".format(
+                len(image_names), size[0], size[1]
+            )
+        )
 
         if not options.skip:
-            self.add_border_to_images(image_names, options.source_directory,
-                                      options.tmp_directory, size)
+            self.add_border_to_images(
+                image_names, options.source_directory, options.tmp_directory, size
+            )
 
         if options.music is not None:
             audio = MP3(options.music)
             fps = len(image_names) / audio.info.length
-            print('Adding soundtrack {}, {}s, {} fps'.format(options.music,
-                                                             audio.info.length,
-                                                             fps))
+            print(
+                "Adding soundtrack {}, {}s, {} fps".format(
+                    options.music, audio.info.length, fps
+                )
+            )
 
         self.make_movie(fps, options.output_filename, options.tmp_directory)
         if options.music is not None:
@@ -77,7 +116,7 @@ class MakeMovie():
     @classmethod
     def save_landscape_images(cls, source_dir, new_image_directory):
         """Save any images that are in landscape mode (width > height)."""
-        valid_extensions = ['png', 'jpg']
+        valid_extensions = ["png", "jpg"]
         for _, _, files in os.walk(source_dir):
             for image in files:
                 ext = image[-3:].lower()
@@ -88,18 +127,19 @@ class MakeMovie():
                 size = orig_image.size
                 if size[0] >= size[1]:
                     new_name = new_image_directory + os.path.basename(image)
-                    print('new_name: {}'.format(new_name))
+                    print("new_name: {}".format(new_name))
                     orig_image.save(new_name)
 
-# Consider using a different library to scale the image more quickly:
-# https://github.com/jbaiter/jpegtran-cffi
-# pylint: disable=too-many-locals
+    # Consider using a different library to scale the image more quickly:
+    # https://github.com/jbaiter/jpegtran-cffi
+    # pylint: disable=too-many-locals
     @classmethod
-    def add_border_to_images(cls, image_list, source_directory,
-                             new_image_directory, size):
+    def add_border_to_images(
+        cls, image_list, source_directory, new_image_directory, size
+    ):
         """Add border to images to make them all the same size."""
         img_num = 1
-        progress = Bar('Processing', max=len(image_list))
+        progress = Bar("Processing", max=len(image_list))
         for timestamp, image in image_list.iteritems():
             new_image = Image.new("RGB", size)
             orig_image = Image.open(image)
@@ -109,9 +149,9 @@ class MakeMovie():
             for orientation in ExifTags.TAGS:
                 if ExifTags.TAGS[orientation] == "Orientation":
                     break
-# pylint: disable=protected-access
+            # pylint: disable=protected-access
             info = dict(orig_image._getexif().items())
-# pylint: enable=protected-access
+            # pylint: enable=protected-access
             if orientation in info:
                 if info[orientation] == 3:
                     orig_image = orig_image.transpose(Image.ROTATE_180)
@@ -123,35 +163,38 @@ class MakeMovie():
                     width, height = height, width
             scale_ratio = 1.0
             if width > size[0] or height > size[1]:
-                scale_ratio = min(size[0] / width,
-                                  size[1] / height)
-                orig_image = orig_image.resize(((int(width *
-                                                     scale_ratio)),
-                                                int(height *
-                                                    scale_ratio)),
-                                               Image.BICUBIC)
-            new_image.paste(orig_image, (int((size[0] - width *
-                                              scale_ratio) / 2),
-                                         int((size[1] - height *
-                                              scale_ratio) / 2)))
+                scale_ratio = min(size[0] / width, size[1] / height)
+                orig_image = orig_image.resize(
+                    ((int(width * scale_ratio)), int(height * scale_ratio)),
+                    Image.BICUBIC,
+                )
+            new_image.paste(
+                orig_image,
+                (
+                    int((size[0] - width * scale_ratio) / 2),
+                    int((size[1] - height * scale_ratio) / 2),
+                ),
+            )
             draw = ImageDraw.Draw(new_image)
             font = ImageFont.truetype(
-                "/usr/share/fonts/truetype/freefont/FreeSans.ttf", 120)
-            date = datetime.strptime(timestamp, '%Y%m%d')
-            date = date.strftime('%B %d, %Y')
+                "/usr/share/fonts/truetype/freefont/FreeSans.ttf", 120
+            )
+            date = datetime.strptime(timestamp, "%Y%m%d")
+            date = date.strftime("%B %d, %Y")
             draw.text((100, 940), date, (255, 255, 255), font=font)
             new_name = image[len(source_directory):]
-            new_name = new_image_directory + '/image_' + \
-                str(img_num).zfill(5) + '.jpg'
+            new_name = new_image_directory + "/image_" + str(img_num).zfill(5) + ".jpg"
             new_image.save(new_name)
             img_num += 1
             progress.next()
         progress.finish()
-# pylint: enable=too-many-locals
+
+    # pylint: enable=too-many-locals
 
     @classmethod
-    def get_max_image_resolution(cls, image_list, absolute_max_width,
-                                 absolute_max_height):
+    def get_max_image_resolution(
+        cls, image_list, absolute_max_width, absolute_max_height
+    ):
         """From a list of images find the maximum height and width."""
         max_height = 0
         max_width = 0
@@ -173,7 +216,7 @@ class MakeMovie():
     @classmethod
     def get_image_names(cls, source_dir):
         """Get the filenames of images in a directory."""
-        valid_extensions = ['png', 'jpg']
+        valid_extensions = ["png", "jpg"]
         image_names = {}
         for _, _, files in os.walk(source_dir):
             for filename in files:
@@ -181,44 +224,45 @@ class MakeMovie():
                 if ext not in valid_extensions:
                     continue
 
-                current_image = Image.open(source_dir+filename)
-# pylint: disable=protected-access
+                current_image = Image.open(source_dir + filename)
+                # pylint: disable=protected-access
                 info = current_image._getexif()
-# pylint: enable=protected-access
+                # pylint: enable=protected-access
                 if info is None:
-                    print('{} has no metadata information.'.format(filename))
+                    print("{} has no metadata information.".format(filename))
                     continue
                 if 36867 in info:
-                    timestamp = str(info[36867]).replace(':', '')[:-7].upper()
+                    timestamp = str(info[36867]).replace(":", "")[:-7].upper()
                 elif 36868 in info:
-                    timestamp = str(info[36868]).replace(':', '')[:-7].upper()
+                    timestamp = str(info[36868]).replace(":", "")[:-7].upper()
                 else:
                     try:
-                        timestamp = \
-                                str(info[306]).replace(':', '')[:-7].upper()
+                        timestamp = str(info[306]).replace(":", "")[:-7].upper()
                     except KeyError as exc:
-                        print('Exception for {}: {}'.format(filename, exc))
+                        print("Exception for {}: {}".format(filename, exc))
                         continue
-                image_names[timestamp] = source_dir+filename
+                image_names[timestamp] = source_dir + filename
 
-        image_names_sorted = \
-            collections.OrderedDict(sorted(image_names.items(),
-                                           key=itemgetter(0)))
+        image_names_sorted = collections.OrderedDict(
+            sorted(image_names.items(), key=itemgetter(0))
+        )
         return image_names_sorted
 
     @classmethod
     def make_movie(cls, fps, output_filename, img_dir):
         """Create a movie using images in a specified directory."""
-        command = ('avconv',
-                   '-r',
-                   str(fps),
-                   '-i',
-                   img_dir+'/image_%05d.jpg',
-                   '-b:v',
-                   '1000k',
-                   output_filename)
+        command = (
+            "avconv",
+            "-r",
+            str(fps),
+            "-i",
+            img_dir + "/image_%05d.jpg",
+            "-b:v",
+            "1000k",
+            output_filename,
+        )
 
-        print("\nabout to execute:\n%s\n" % ' '.join(command))
+        print("\nabout to execute:\n%s\n" % " ".join(command))
         subprocess.check_call(command)
 
     @classmethod
@@ -236,24 +280,26 @@ class MakeMovie():
         ffmpeg -i tmp_MP3WRAP.mp3 -acodec copy all.mp3 && rm tmp_MP3WRAP.mp3
         id3cp 1.mp3 all.mp3
         """
-        command = ('avconv',
-                   '-i',
-                   output_filename,
-                   '-i',
-                   music,
-                   '-map',
-                   '0:0',
-                   '-map',
-                   '1:0',
-                   '-vcodec',
-                   'copy',
-                   '-acodec',
-                   'copy',
-                   'music-' + output_filename)
+        command = (
+            "avconv",
+            "-i",
+            output_filename,
+            "-i",
+            music,
+            "-map",
+            "0:0",
+            "-map",
+            "1:0",
+            "-vcodec",
+            "copy",
+            "-acodec",
+            "copy",
+            "music-" + output_filename,
+        )
 
-        print("\nabout to execute:\n%s\n" % ' '.join(command))
+        print("\nabout to execute:\n%s\n" % " ".join(command))
         subprocess.check_call(command)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     MakeMovie()
--- rename_file_with_date_time.py
+++ rename_file_with_date_time.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 """
@@ -12,41 +12,48 @@ instead of date in filenames.
 """
 
 
-from __future__ import print_function
-
 from argparse import ArgumentParser
 import os
 from shutil import copyfile
 from PIL import Image
 
 
-class RenameFileWithDateTime():
+class RenameFileWithDateTime:
     """Rename image files with date and time for easier sorting."""
 
     def __init__(self):
         """Rename file."""
         parser = ArgumentParser()
-        parser.add_argument("-s", "--source_dir", dest="source_directory",
-                            help="Directory containing original images. "
-                                 "Default to current directory.", default='.')
-        parser.add_argument("-t", "--tmp_dir", dest="tmp_directory",
-                            help="Directory containing temporary images. "
-                                 "Default to tmp.", default='tmp')
+        parser.add_argument(
+            "-s",
+            "--source_dir",
+            dest="source_directory",
+            help="Directory containing original images. "
+            "Default to current directory.",
+            default=".",
+        )
+        parser.add_argument(
+            "-t",
+            "--tmp_dir",
+            dest="tmp_directory",
+            help="Directory containing temporary images. " "Default to tmp.",
+            default="tmp",
+        )
         options = parser.parse_args()
         source_dir = options.source_directory
         new_image_directory = options.tmp_directory
-        # pylint: disable=no-member
-        num_files = len(os.walk(source_dir).next()[2])
-        # pylint: enable=no-member
+        num_files = len(
+            [name for name in os.listdir(source_dir) if os.path.isfile(name)]
+        )
 
         if num_files > 0:
             self.create_files(source_dir, new_image_directory)
         else:
-            print('No files in {}'.format(source_dir))
+            print("No files in {}".format(source_dir))
 
     def create_files(self, source_dir, tmp_dir):
         """Create the files using metadata."""
-        valid_extensions = ['png', 'jpg']
+        valid_extensions = ["png", "jpg"]
         for _, _, files in os.walk(source_dir):
             for filename in files:
                 ext = filename[-3:].lower()
@@ -57,39 +64,41 @@ class RenameFileWithDateTime():
     @classmethod
     def create_file(cls, filename, source_dir, tmp_dir):
         """Create individaul files using metadata."""
-        print('Opening {}'.format(filename))
+        print("Opening {}".format(filename))
         if os.path.isfile(source_dir + filename):
-            current_image = Image.open(source_dir+filename)
+            current_image = Image.open(source_dir + filename)
             # pylint: disable=W0212
             info = current_image._getexif()
             # pylint: enable=W0212
             if 36867 in info:
-                if info[272] == 'HTC6525LVW':
-                    print('info[36867] =', info[36867])
-                    new_filename = \
-                        str(info[36867]).replace(':', '').upper()
-                    new_filename = \
-                        new_filename.replace(' ', '_')
+                if info[272] == "HTC6525LVW":
+                    print("info[36867] =", info[36867])
+                    new_filename = str(info[36867]).replace(":", "").upper()
+                    new_filename = new_filename.replace(" ", "_")
                 else:
-                    new_filename = \
-                        str(info[36867]).replace(':', '')[:-7].upper() + \
-                        '_' + str(info[36867]).replace(':', '')[9:16].upper()
+                    new_filename = (
+                        str(info[36867]).replace(":", "")[:-7].upper()
+                        + "_"
+                        + str(info[36867]).replace(":", "")[9:16].upper()
+                    )
             else:
-                new_filename = \
-                    str(info[306]).replace(':', '')[:-7].upper()
-            new_filename = \
-                tmp_dir + '/' + 'IMG_' + new_filename + '.jpg'
-            print('Original filename = {}, new filename = '
-                  '{}'.format(filename, new_filename))
+                new_filename = str(info[306]).replace(":", "")[:-7].upper()
+            new_filename = tmp_dir + "/" + "IMG_" + new_filename + ".jpg"
+            print(
+                "Original filename = {}, new filename = "
+                "{}".format(filename, new_filename)
+            )
             try:
-                copyfile(source_dir+'/'+filename, new_filename)
+                copyfile(source_dir + "/" + filename, new_filename)
             except IOError:
-                print('Exception caught renaming {} --> '
-                      '{}'.format(filename, new_filename))
+                print(
+                    "Exception caught renaming {} --> "
+                    "{}".format(filename, new_filename)
+                )
         else:
-            print('{} is not a file, skipping'.format(filename))
-        print('*****')
+            print("{} is not a file, skipping".format(filename))
+        print("*****")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     RenameFileWithDateTime()
